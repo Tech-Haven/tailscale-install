8eb792d950ffc02b870d53c976cadd8c8f2c96eb
Datallboy <github@datallboy.com>
2023-09-20 10:38:57 -0400

    Updated comments to describe flags

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 curl -fsSL https://tailscale.com/install.sh | sudo sh
 
 # Connect to Tech Haven tailnet
-# Will provide a link for OIDC authentication
+# --login-server will provide a link for OIDC authentication
+# --accept-routes accepts subnet route 10.100.0.0/16 advertised from the lab
 sudo tailscale up --login-server https://headscale.techhaven.io --accept-routes
